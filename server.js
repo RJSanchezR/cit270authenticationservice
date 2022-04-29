ee7015e6e6b6d3e4a0078a21fbb5642a7519e1ad
RJSanchezR <san16071@byui.edu>
2022-04-29 13:51:43 -0600

    Added new files

---
+++ server.js
@@ -0,0 +1,7 @@
+const express  = require('express'); //Importing the library
+
+const app = express(); //Using the library
+
+app.listen(3000,()=>{console.log("listening...")}); //Listen
+
+app.get('/',(req,res)=>{res.send("Hello")}); //Respond
