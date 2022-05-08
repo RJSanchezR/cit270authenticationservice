ec5f4b91cc25cfbc3450ee357b1b35606deecf56
RJSanchezR <san16071@byui.edu>
2022-05-07 22:24:45 -0600

    changes in the server

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express'); //Importing the library
-
+const bodyParser = require('body-parser'); //Middleware
+const port = 3000;
 const app = express(); //Using the library
 
-app.listen(3000,()=>{console.log("listening...")}); //Listen
+app.use(bodyParser.json()); //Using the middleware
 
-app.get('/',(req,res)=>{res.send("Hello")}); //Respond
+app.listen(port, ()=>{
+    console.log("listening on port: "+port)
+})
+
+app.post('/login', (request, response) =>{
+    const loginRequest = request.body;
+    if (loginRequest.userName == "tiyiwe2325@reimondo.com" && loginRequest.password == "MiPassword1!"){
+        response.status(200);
+        response.send("Welcome buddy");
+    } else {
+        response.status(401);
+        response.send("Unauthorized ma fren");
+    }
+});
+
+app.get('/',(request, response) => response.send("Hello"));
