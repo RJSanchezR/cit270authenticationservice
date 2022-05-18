19ec3566022e20ef5be133878ba1745c6a4ad913
RJSanchezR <san16071@byui.edu>
2022-05-18 13:59:17 -0600

    Made the API work

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
 const express = require('express'); //Importing the library
-const bodyParser = require('body-parser'); //Middleware
+
 const port = 3000;
-const md5 = require('md5');
-const app = express(); //Using the library
-const redis = require('redis');
+const app = express(); // Using the library
+const md5 = require('md5'); // Importing the library
+const bodyParser = require('body-parser'); // Middleware
+const {createClient} = require('redis');
+const redisClient = createClient(
+{
+    socket:{
+        port:6379,
+        host:"127.0.0.1",
+    }
+}
+); // This creates a connection to the redis database
+redisClient.connect();
 
-const redisClient = redis.createClient();
-
-app.use(bodyParser.json()); //Using the middleware
+app.use(bodyParser.json()); // Using the middleware (call it before anything else happens on each request)
 
 app.listen(port, ()=>{
-    console.log("listening on port: "+port)
+    console.log("Listening on port: "+port);
 })
 
-app.post('/login',async (request, response) =>{
-
-    const hashedPassword = md5(request.body.password);
-    const password = await redisClient.hGet('credentials', request.body.userName);
+const validatePassword = async (request, response)=>{
+    //await redisClient.connect(); // Creating a TCP socket with Redis
+    const requestHashedPassword = md5(request.body.password);
+    const redisHashedPassword = await redisClient.hmGet('credentials', request.body.userName); // Read password from Redis
     const loginRequest = request.body;
-    console.log("Request body", JSON.stringify(request.body));
-    //Search database for username, and retrieve current password
+    console.log("Request Body",JSON.stringify(request.body)); // Search the database for username and retrieve current password
 
-    //Compared the hashed version of the password that has sent with the hashed version from the database
-    if (loginRequest.userName == "tiyiwe2325@reimondo.com" && loginRequest.password == "Password1"){
+    // Compare the hashed version of the password that was sent with the hashed version from the database
+    if(requestHashedPassword == redisHashedPassword){
         response.status(200); //200 means ok
-        response.send("Welcome buddy");
+        response.send("Welcome mijo");
     } else {
         response.status(401); //400 means unauthorized
         response.send("Unauthorized ma fren");
     }
-});
+}
 
-app.get('/',(request, response) => response.send("Hello"));
+app.get('/',(request,response)=>{ // Every time something calls your API that is a request
+    response.send("Hello"); // A response is when the API gives the information requested
+})
+
+app.post('/login',validatePassword);
