235e9c7972763ef20edde166ad5a71c929c269c9
RJSanchezR <san16071@byui.edu>
2022-06-29 13:15:58 -0600

    changed redis ip

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser'); // Middleware
 const {createClient} = require('redis');
 const redisClient = createClient(
 {
-    url: 'redis://default:mypassword1@10.128.0.2:6379',
+    url: 'redis://default:mypassword1@104.197.6.64:6379',
     socket:{
         port:6379,
         host:"127.0.0.1",
